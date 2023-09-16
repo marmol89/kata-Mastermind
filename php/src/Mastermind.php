7928bff97866ec3f91092dc858a247b69d247b58
adrian <adrian.marin.chorro@gmail.com>
2023-09-16 19:35:46 +0200

    REFACTOR

--- php/src/Mastermind.php
+++ php/src/Mastermind.php
@@ -6,9 +6,18 @@ class Mastermind
 {
     public function evaluate(array $codeMaker, array $codeBreaker): array
     {
-        if ($codeBreaker === $codeMaker) return [count($codeBreaker),0];
-        if ($codeBreaker == ['blue','blue']) return [1,0];
-        if ($codeMaker === ['blue','red']) return [0,2];
-        return [0,0];
+        $positionAndColor = 0;
+        $color = 0;
+
+       foreach($codeMaker as $indice => $valor){
+            if ($codeBreaker[$indice] == $valor) {
+                $positionAndColor++;
+                unset($codeMaker[$indice]);
+            } else if (in_array($valor, $codeBreaker)) {
+                $color++;
+            }
+        }
+
+        return [$positionAndColor, $color];
     }
 }
